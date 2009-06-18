4eda5def360794e24bfb71d0a4739263f72c176c
Austin Moore <austin.moore@mac.com>
2009-06-18 15:17:56 +0200

    update readme

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_linde-prototype_session',
+  :key         => '_ttalk-app_session',
   :secret      => '1ab372c948e16221c72a038805b267dca189475f7a09404f49e51e0b417343051a5977200f799f879a4d764a939cc4dd552cbb7f68f6d39d4b82b0d6fbb7ae18'
 }
 
